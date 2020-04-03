9d2dd515d32e0d72095f57f9adae6e51df04744f
Alexandre Bédard <alexandrebedard.xyz>
2020-04-03 18:07:49 -0400

    3.4 mvvm

---
+++ Inventaire/CustomerView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using BillingManagement.UI.ViewModels;
+using System.Windows;
+
+namespace Inventaire
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(CustomersViewModel vm)
+        {
+            InitializeComponent();
+
+            DataContext = vm;
+        }
+    }
+}
